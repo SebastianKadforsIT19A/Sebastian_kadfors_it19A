ecfcf4e13cf02fb8104fb7b1ecfd1318d93db824
SebastianKadforsIT19A <70194370+SebastianKadforsIT19A@users.noreply.github.com>
2020-09-03 09:56:06 +0200

    All is addition on the webbsite

--- Introkod/index.js
+++ Introkod/index.js
@@ -10,3 +10,31 @@ if (age >= 18) {
 }
 let text = document.querySelector("#text")
 text.innerText = `${name} är ${age} är gammal`
+let multi = document.querySelector("#multiplikation")
+let div = document.querySelector("#division")
+let add = document.querySelector("#addition")
+let sub = document.querySelector("#subtraktion")
+
+multi.innerHTML = `multiplikation`
+div.innerHTML = `divsion`
+add.innerHTML = `addition`
+sub.innerHTML = `subtraktion`
+
+//skapar variabel för knappen
+let button = document.querySelector("#knapp")
+
+//lägger till en eventyssnare till knappen
+
+button.addEventListener("click", math)
+
+function math(event){
+    console.log("math-funktionen körs")
+
+    let num1 = parseFloat(document.querySelector("#number1").value)
+    let num2 = parseFloat(document.querySelector("#number2").value)
+
+    add.innerHTML = `${num1}+${num2}=${num1+num2}`
+    sub.innerHTML = `${num1}+${num2}=${num1+num2}`
+    multi.innerHTML = `${num1}+${num2}=${num1+num2}`
+    div.innerHTML = `${num1}+${num2}=${num1+num2}`
+}
