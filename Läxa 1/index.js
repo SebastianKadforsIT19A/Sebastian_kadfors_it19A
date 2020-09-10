ff94c5dde84d209a5c2b0230704dd85dfbcca781
SebastianKadforsIT19A <70194370+SebastianKadforsIT19A@users.noreply.github.com>
2020-09-10 10:20:17 +0200

    Answer square done

---
+++ Läxa 1/index.js
@@ -0,0 +1,13 @@
+
+
+
+
+let Namn  = prompt("Hur gammal är du?")
+let Alder = prompt("Va heter du?")
+let Adress = prompt("Var bor du?")
+let Telefon_nummer = prompt("Vad är ditt nummer? :)")
+
+console.log (`${Namn}`)
+console.log (`${Alder}`)
+console.log (`${Adress}`)
+console.log (`${Telefon_nummer}`)
---
+++ Variabel_io/idenx.js
@@ -0,0 +1,41 @@
+// skapa en variabel name och tilldela det ditt namn
+name= "Sebastian"
+
+
+
+// skriv ut "Hej namn"
+console.log (`Hej ${name}`)
+
+
+
+// 1. skapa variablerna tal1, tal2, tal3
+// 2. tilldela dessa variabler med värderna 3,4,5
+let tal1 = 3 
+let tal2 = 4
+let tal3 = 5
+
+// 3. räkna ut medelvärdet av dem
+medelvärdet = (tal1+tal2+tal3)/3
+
+// 4. Gör förljande utskrift:
+//"Medelvärdet av talen 3,4,5 är"
+
+
+
+
+
+
+
+console.log(`(${tal1}+${tal2}+${tal3})/3 = ${medelvärdet}`)
+
+let langd = prompt("ange värdet för rektanglns längd ")
+
+
+//area på en rektangel
+let langd = prompt("Ange rektangelns längd")
+
+let bredd = prompt("Ange rektangelns bredd")
+
+let area = langd*bredd
+
+console.log(`Arean är ${area} a.e.`)
