d21b3d9a8e793e528674c05ecc24b5d4c6d1d2c6
SebastianKadforsIT19A <70194370+SebastianKadforsIT19A@users.noreply.github.com>
2020-09-17 12:07:50 +0200

    if_sats2

---
+++ if_sats2.js
@@ -0,0 +1,14 @@
+/* SKriv ett program som låter användaren mata in sin ålder. 
+Programmet ska avgöra om personen får kolla på en 15-årsgräns film på bio /*
+
+
+/* Utöka programmet till att kolla om användaren får titta på en 7-årsgräns film på bio*/
+
+
+/*Utöka programmet till att kolla om användaren får titta på en 18-årsgräns film på bio*/
+
+
+let age= prompt ("Ange ålder")
+
+
+prompt ()
