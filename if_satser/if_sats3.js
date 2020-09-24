159336cb55052d21cb501e77e3867ea822dafb70
SebastianKadforsIT19A <70194370+SebastianKadforsIT19A@users.noreply.github.com>
2020-09-24 09:57:46 +0200

    for1 on point

---
+++ if_satser/if_sats3.js
@@ -0,0 +1,14 @@
+let age = 1-10
+
+
+if (age <8){
+    console.log ("Den är för tung")
+} else { (age >8) 
+    console.log ("Den är inte för tung")  
+}
+
+let name = prompt ("Hur mycket väger bagaget?")
+
+
+
+console.log (`${name}`)
