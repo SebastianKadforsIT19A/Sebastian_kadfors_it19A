487969b561bba900287ce6a81c3a7c3b9550fdeb
SebastianKadforsIT19A <70194370+SebastianKadforsIT19A@users.noreply.github.com>
2020-09-29 11:05:19 +0200

    idenx och for1.js almost fixed

--- for1/for1.js
+++ for1/for1.js
@@ -3,11 +3,12 @@ console.log ("hej")
 
 let p_talserie1 = document.querySelector("#talserie1")
 let p_talserie2 = document.querySelector("#talserie2")
-let p_summa = docuemnt.querySelector ("#summa")
+let p_summa = document.querySelector ("#summa")
+
+let summa = 0
 
 
-
-for (let j=10; j>=0; j--){
+for (let j=10; j>=0; j++){
     p_talserie1.innerHTML += `${j}`
 
 }
@@ -17,7 +18,7 @@ for (let i=1; i<=10; i++) {
 }
 
 
-for(let i = 0; i<5; i++){
+for(let i = 0; i<10; i++){
     console.log(`${i} I will not write all over the walls`)
 }
 
@@ -25,5 +26,16 @@ for(let i = 0; i<5; i++){
 for (let i=0; i<10; i++){
     summa += 1
 }
-
+p_summa.innerHTMLn += `1 + 2 + 3 +... +9 +10 = ${summa}`
 console.log(summa)
+
+//skapa en lista med elever
+let elver = ["Sebsatian", "Elis", "Max", "Linus", "Lukas", "Albin"]
+
+p_klass.innerHTML = "Klass IT19A innehåller följande elever: <br/>"
+
+console.log(elever.length)
+
+for (let k = 0; k < elever.length; k++){
+    p_klass.innetHTML += `${k+1}. ${elver[k]} <br/l>`
+}
