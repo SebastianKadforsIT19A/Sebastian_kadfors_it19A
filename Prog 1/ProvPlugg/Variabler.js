e71345b3a9f0d07e8ea42ca72383cadda7485b21
SebastianKadforsIT19A <70194370+SebastianKadforsIT19A@users.noreply.github.com>
2020-11-03 00:56:11 +0100

    first almost by my self variabler

--- Prog 1/ProvPlugg/Variabler.js
+++ Prog 1/ProvPlugg/Variabler.js
@@ -8,16 +8,14 @@ let tal3 = 5
 
 medelvärde = (tal1+tal2+tal3)/3
 
-// 4. Gör förljande utskrift:
-//"Medelvärdet av talen 3,4,5 är"
-console.log(`${tal1}+${tal2}+${tal3})/3 = ${medelvärde}`)
+console.log (`${tal1}+${tal2}+${tal3})/3 = ${medelvärde}`)
 
-//area på en rektangel
 
 let langd = prompt ("Ange längden på rektangeln")
 let djup = prompt ("Ange djupet på rektangeln")
-let bredd = prompt ("Ange bredden på rektangeln")
+let bredd = prompt ("Ange bredden på rekatngeln")
+
 
 let area = langd*djup*bredd
 
-console.log (`Arean är ${area} a.e`)
+console.log(`Arean är ${area} a.e`)
