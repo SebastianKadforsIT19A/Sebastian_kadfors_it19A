9c3b5afe1648384b52a7ea2d7be12a5f301a7611
SebastianKadforsIT19A <sebastian.kadfors@elev.ga.ntig.se>
2020-11-09 20:42:57 +0100

    Style.css

--- Prog 1/App/app.js
+++ Prog 1/App/app.js
@@ -1,47 +1,49 @@
 let p_siffra = document.querySelector("#siffra");
 let upp = document.querySelector("#uppKnapp");
 let ned = document.querySelector("#nedKnapp");
-let reset = document.querySelector("#reset");
+let reset = document.querySelector("#resetKnapp");
+let p_tecken = document.querySelector("#tecken");
 
-let siffra = 0; //ändra vid knapptryckingar
+let siffra = 0; // ändra vid knapptryckningar
+
 p_siffra.innerHTML = siffra;
+p_tecken.innerHTML = "Noll";
 
+upp.addEventListener("click", raknaUpp);
+ned.addEventListener("click", raknaNed);
+reset.addEventListener("click", resetNummer);
 
-upp.addEventlistener("click", raknaUpp);
-
-function raknaupp () {
-    console.log("Räkna upp knapp tryck");
-    // öka värdet på siffra
-    siffra = siffra + 1;
-    console.log(siffra);
-    //skriva ut den i p_siffra
-    p_siffra.innerHTML = siffra;
-    teckenCheck();
+function raknaUpp() {
+  // öka värdet på siffra
+  siffra = siffra + 1;
+  // skriva ut den i p_siffra
+  p_siffra.innerHTML = siffra;
+  teckenCheck();
 }
 
-//skapa funktion för att räkna ned
-function raknaNed(){
-siffra = siffra - 1;
-p_siffra.innerHTML = siffra;
-teckenCheck();
+// skapa funktion för att räkna ned
+function raknaNed() {
+  siffra = siffra - 1;
+  p_siffra.innerHTML = siffra;
+  teckenCheck();
 }
 
-
-// skapa funktion för att nollställa 
+// skapa funktion för att nollställa
 function resetNummer() {
-    siffra = 0 
-    p_siffra.innerHTML=0
-    teckenCheck();
-
-
+  siffra = 0;
+  p_siffra.innerHTML = 0;
+  teckenCheck();
 }
 
 function teckenCheck() {
-    if (siffra < 0) {
-    p_tecken.innetHTML ="Negativt";
-} else if (siffra > 0) {
+  if (siffra < 0) {
+    p_tecken.innerHTML = "Negativt";
+    p_siffra.style.color = "darkred";
+  } else if (siffra > 0) {
     p_tecken.innerHTML = "Positivt";
-} else {
-    ptecken.innerHTML = "Noll";
-}
+    p_siffra.style.color = "green";
+  } else {
+    p_tecken.innerHTML = "Noll";
+    p_siffra.style.color = "blue";
+  }
 }
