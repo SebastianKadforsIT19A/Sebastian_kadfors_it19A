2354cec2fd41e1f97df5f41d0e118a3d7c283bed
SebastianKadforsIT19A <70194370+SebastianKadforsIT19A@users.noreply.github.com>
2020-11-19 10:16:19 +0100

    for och word counter done

--- Prog 1/for1/for1.js
+++ Prog 1/for1/for1.js
@@ -1,22 +1,32 @@
-let p_talserie1 = document.querySelector("#talserie1")
-let p_talserie2 = document.querySelector("#talserie2")
-let p_summa = document.querySelector("#summa")
-let p_klass = document.querySelector("#klasslista")
-let summa = 0
+let p_talserie1 = document.querySelector("#talserie1");
+let p_talserie2 = document.querySelector("#talserie2");
+let p_summa = document.querySelector("#summa");
+let p_lektioner = document.querySelector("#lektioner");
+let summa = 0;
 
-for (let i=1; i<=10; i++){
-@@ -20,3 +21,17 @@} for= (let i=0; i<=10; i++) {
-    summa += 1
+for (let i= 1; i <=50; i++) {
+  p_talserie1.innerHTML +=${2 * i}, ;
+  p_talserie2.innerHTML += ${2 * i + 1}, ;
 }
-p_summa.innerHTML += `1 + 2 +3 +...+9+10 = ${summa}`
 
+p_summa.innerHTML += 1 + 2 + 3 +...+ 9 + 10;
+console.log(summa);
 
-// skapa en lista med elever
-let elever = ["Kokchun", "Henrik", "Thomas", "Tatiana", "Zsofia", "David"]
+for (let i = 0; i < 100; i++) {
+  console.log(${i} i will write all over the wall);
+}
 
-p_klass.innerHTML = "Klass IT19A innehåller följande elever: <br/>"
-
-console.log(elever.length)
-
-for (let k = 0; k < elever.length; k++){
-    p_klass.innerHTML += `${k+1}. ${elever[k]} <br/>`}
+var lektioner = [
+  "nätverksteknik",
+  "datorteknik",
+  "tillämpladprogramering",
+  "engelska",
+  "svenska",
+  "programering",
+];
+var text = "hej";
+var i;
+for (i = 0; i < lektioner.length; i++) {
+  text += lektioner[i] + "<br>";
+}
+document.getElementById("lektioner").innerHTML = text;
