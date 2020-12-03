cd004c489a8357e0ab7d2e80c662b3383ee527ff
SebastianKadforsIT19A <70194370+SebastianKadforsIT19A@users.noreply.github.com>
2020-12-03 09:23:30 +0100

    For1 done

---
+++ Prog 1/Randomizer/Random1.js
@@ -0,0 +1,15 @@
+console.log ("Random hej")
+let slumptal = Math.random()
+
+console.log (slumptal)
+
+let coinflip = Math.round(slumptal)
+
+console.log(`Coin flip: ${coinflip}`)
+
+
+if  (slumptal = 0)  {
+    console.log (tails)
+} else {
+    console.log (head)
+}
--- Prog 1/for1/for1.js
+++ Prog 1/for1/for1.js
@@ -3,19 +3,19 @@ let p_talserie2 = document.querySelector("#talserie2");
 let p_summa = document.querySelector("#summa");
 let p_lektioner = document.querySelector("#lektioner");
 let summa = 0;
-
-for (let i= 1; i <=50; i++) {
-  p_talserie1.innerHTML +=${2 * i}, ;
-  p_talserie2.innerHTML += ${2 * i + 1}, ;
+//For satserna här tar då 0 och gångrar den me 2, och för att kunna få de ojämnatalen så plusar man på en etta 
+for (let i= 0; i <=50; i++) {
+  p_talserie1.innerHTML += ` ${2 * i }` ;
+  p_talserie2.innerHTML += ` ${2 * i + 1 }` ;
 }
 
-p_summa.innerHTML += 1 + 2 + 3 +...+ 9 + 10;
+p_summa.innerHTML += 0, + 1, + 2, + 3, + + 9, + 10;
 console.log(summa);
 
 for (let i = 0; i < 100; i++) {
   console.log(`${i} i will write all over the wall`);
 }
-
+//Denna beskriver vilka lektioner vi har med hjälp av variabler
 var lektioner = [
   "nätverksteknik",
   "datorteknik",
@@ -24,9 +24,11 @@ var lektioner = [
   "svenska",
   "programering",
 ];
-var text = "hej";
+
+var text = "";
 var i;
 for (i = 0; i < lektioner.length; i++) {
   text += lektioner[i] + "<br>";
 }
+//Denna skriver ut lektionerna så man kan se dem
 document.getElementById("lektioner").innerHTML = text;
