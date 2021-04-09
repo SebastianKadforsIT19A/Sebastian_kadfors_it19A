0fe0e5ffdf4c6b062c70b4e2c966e7ca5341422b
SebastianKadforsIT19A <70194370+SebastianKadforsIT19A@users.noreply.github.com>
2021-04-10 00:42:28 +0200

    Repition before retest

---
+++ Repetition v10/repetition.js
@@ -0,0 +1,23 @@
+console.log("Repetition v10")
+
+//Skapa variabler
+let namn = "Sebatian"
+let ålder = 17
+
+console.log (`Hej ${namn}, du är ${ålder} år gammal`)
+
+// if sats
+if (ålder >= 18) {
+    console.log("Du är vuxen")
+} else {
+    console.log("Du är barn");
+} 
+
+for (let i = 0; i < 5; i++){
+    console.log(i)
+}   
+
+
+let frukter = ["kiwi", "äpplen", "apelsiner", "jordgubbar"]
+let godFrukt = frukter [2]
+console.log (godFrukt)
